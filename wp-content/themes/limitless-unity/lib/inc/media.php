6f1494a6dadf67a6998f2b6455b2f3364fa65d28
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2013-12-23 19:26:54 -0500

    fix izing again

--- wp-content/themes/limitless-unity/lib/inc/media.php
+++ wp-content/themes/limitless-unity/lib/inc/media.php
@@ -2,7 +2,7 @@
 /**
  * Add new image sizes
  */
-add_image_size('page-header-image', 730, 220, TRUE);
+add_image_size('page-header-image', 760, 230, TRUE);
 add_image_size('post-thumb', 225, 160, TRUE);
 add_image_size( 'post-image', 760, 230, TRUE ); //image to float at the top of the post. Reversed Out does these a lot.
 
