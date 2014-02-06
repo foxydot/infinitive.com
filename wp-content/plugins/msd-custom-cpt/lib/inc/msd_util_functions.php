1287e85d0317b08508ed8050b310d00def016155
msdlab <catherine@madsciencedept.com>
2014-02-06 14:17:12 -0700

    sync

--- wp-content/plugins/msd-custom-cpt/lib/inc/msd_util_functions.php
+++ wp-content/plugins/msd-custom-cpt/lib/inc/msd_util_functions.php
@@ -136,7 +136,7 @@ if(!function_exists('msd_str_fmt')){
     function msd_str_fmt($str,$format = FALSE){
         switch($format){
             case 'email':
-                $ret = '<a href="mailto:'.antispambot($str).'?CC='.antispambot('info@rendigs.com').'" class="email">'.antispambot($str).'</a>';
+                $ret = '<a href="mailto:'.antispambot($str).'" class="email">'.antispambot($str).'</a>';
                 break;
             case 'phone':
                 $str = preg_replace("/[^0-9]/", "", $str);
