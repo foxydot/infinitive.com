33f098ecd3806ff9532a58d8449afc9c04e60963
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-02-06 16:14:43 -0500

    responsive

--- wp-content/themes/limitless-unity/lib/js/theme-jquery.js
+++ wp-content/themes/limitless-unity/lib/js/theme-jquery.js
@@ -54,3 +54,41 @@ jQuery(document).ready(function($) {
     $('.nav-secondary .menu-item .sub-menu').wrap('<div class="subnav-wrapper"></div>');
 	
 });
+
+jQuery(document).ready(function(){
+
+/*RESPONSIVE NAVIGATION, COMBINES MENUS EXCEPT FOR FOOTER MENU*/
+
+    jQuery('.menu-primary, .menu-secondary').wrap('<div id="nav-response" class="nav-responsive">');
+    jQuery('header.site-header').after('<div id="mobile-nav"><a href="#" id="pull"><strong>MENU</strong></a></div>');  
+    
+    //combinate
+    sf_duplicate_menu( jQuery('.nav-responsive>ul'), jQuery('#mobile-nav #pull'), 'mobile_menu', 'sf_mobile_menu' );
+                
+    function sf_duplicate_menu( menu, append_to, menu_id, menu_class ){
+        var jQuerycloned_nav;
+        
+        menu.clone().attr('id',menu_id).removeClass().attr('class',menu_class).appendTo( append_to );
+        jQuerycloned_nav = append_to.find('> ul');
+        jQuerycloned_nav.find('.menu_slide').remove();
+        jQuerycloned_nav.find('li:first').addClass('sf_first_mobile_item');
+        
+        append_to.addClass('closed');
+        jQuerycloned_nav.slideUp( 500 );
+        
+        append_to.click( function(){
+            if ( jQuery(this).hasClass('closed') ){
+                jQuery(this).removeClass( 'closed' ).addClass( 'opened' );
+                jQuerycloned_nav.slideDown( 500 );
+            } else {
+                jQuery(this).removeClass( 'opened' ).addClass( 'closed' );
+                jQuerycloned_nav.slideUp( 500 );
+            }
+            return false;
+        } );
+        
+        append_to.find('a').click( function(event){
+            event.stopPropagation();
+        } );
+    }
+});
