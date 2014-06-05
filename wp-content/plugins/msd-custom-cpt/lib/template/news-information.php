82990f495375ced5b990f8feaeb928a463f9f5e3
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-06-05 15:50:03 -0400

    clear extra junk

--- wp-content/plugins/msd-custom-cpt/lib/template/news-information.php
+++ wp-content/plugins/msd-custom-cpt/lib/template/news-information.php
@@ -1,8 +1,6 @@
 <?php global $wpalchemy_media_access; ?>
 <div class="my_meta_control gform_wrapper">
 <ul class='gform_fields top_label description_below'>
-<?php $mb->the_field('youtube'); ?>
-<div class='ginput_container'><input name='<?php $mb->the_name(); ?>' type='text' value='<?php $mb->the_value(); ?>' class='medium'  tabindex='3'  /></div></li>
 <li class='gfield' ><label class='gfield_label'>Team Members</label>
 <div class='ginput_container'>
     <?php 
