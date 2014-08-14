5f57b469b3d70b75662154b024f5755b109b9148
msdlab <catherine@madsciencedept.com>
2014-08-14 14:52:16 -0600

    update msd image plugin

--- wp-content/plugins/msd-image-widget/views/widget.php
+++ wp-content/plugins/msd-image-widget/views/widget.php
@@ -6,44 +6,23 @@
 
 // Block direct requests
 if ( !defined('ABSPATH') )
-	die('-1');
+    die('-1');
 
 echo $before_widget;
-if ( !empty( $title ) ) { echo $before_title . $title . $after_title; }
-if ( !empty( $image ) ) {
-	if ( $link ) {
-		echo '<a class="'.$this->widget_options['classname'].'-image-link" href="'.esc_url($link).'" target="'.esc_attr($linktarget).'">';
-	}
-	if ( $imageurl ) {
-		echo '<img src="'.esc_url($imageurl).'" style="';
-		if ( !empty( $width ) && is_numeric( $width ) ) {
-			echo "max-width: {$width}px;";
-		}
-		if ( !empty( $height ) && is_numeric( $height ) ) {
-			echo "max-height: {$height}px;";
-		}
-		echo "\"";
-		if ( !empty( $align ) && $align != 'none' ) {
-			$align = esc_attr($align);
-			echo " class=\"align{$align}\"";
-		}
-		if ( !empty( $alt ) ) {
-			$alt = esc_attr($alt);
-			echo " alt=\"{$alt}\"";
-		} else {
-			$title = esc_attr($title);
-			echo " alt=\"{$title}\"";					
-		}
-		echo " />";
-	}
 
-	if ( $link ) { echo '</a>'; }
-}
+if ( !empty( $title ) ) { echo $before_title . $title . $after_title; }
+echo '<div class="widget-content">';
+echo $this->get_image_html( $instance, true );
+
 if ( !empty( $description ) ) {
-	$text = apply_filters( 'widget_text', $description, $args, $instance );
-	echo '<div class="'.$this->widget_options['classname'].'-description" >';
-	echo wpautop( $text );			
-	echo "</div>";
+    echo '<div class="'.$this->widget_options['classname'].'-description" >';
+    echo wpautop( $description );
+    echo "</div>";
 }
+if ( $link ) {
+    $linktext = $linktext != ''?$linktext:'Read More >';
+    echo '<div class="link"><a class="'.$this->widget_options['classname'].'-link readmore" href="'.$link.'" target="'.$linktarget.'">'.$linktext.' ></a><div class="clear"></div></div>';
+}
+echo '<div class="clear"></div>
+</div>';
 echo $after_widget;
-?>
