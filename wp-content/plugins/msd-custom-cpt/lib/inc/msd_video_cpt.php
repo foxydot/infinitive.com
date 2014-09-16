49a32773023a8e863f07367482d29998b92e29d0
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2014-09-16 10:38:55 -0400

    access to menu order for video cpt

--- wp-content/plugins/msd-custom-cpt/lib/inc/msd_video_cpt.php
+++ wp-content/plugins/msd-custom-cpt/lib/inc/msd_video_cpt.php
@@ -103,7 +103,7 @@ class MSDVideoCPT {
             'labels' => $labels,
             'hierarchical' => true,
             'description' => 'Customer Videos',
-            'supports' => array( 'title', 'editor', 'author', 'thumbnail'),
+            'supports' => array( 'title', 'editor', 'author', 'thumbnail','page-attributes'),
             'taxonomies' => array('msd_video_tag'),
             'public' => true,
             'show_ui' => true,
