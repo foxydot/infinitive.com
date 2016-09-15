4c71927c8feb89b2c9a5f0e7830129f2e1e1efe3
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2016-09-15 19:02:50 -0400

    create white rabbit

---
+++ assets/themes/whiterabbit/lib/js/theme-jquery.js
@@ -0,0 +1,42 @@
+jQuery(document).ready(function($) {	
+    $('p:empty').remove();
+    $('*:first-child').addClass('first-child');
+    $('*:last-child').addClass('last-child');
+    $('*:nth-child(even)').addClass('even');
+    $('*:nth-child(odd)').addClass('odd');
+    $('body').css('opacity','1');
+	
+	var numwidgets = $('#footer-widgets div.widget').length;
+	$('#footer-widgets').addClass('cols-'+numwidgets);
+	$.each(['show', 'hide'], function (i, ev) {
+        var el = $.fn[ev];
+        $.fn[ev] = function () {
+          this.trigger(ev);
+          return el.apply(this, arguments);
+        };
+      });
+
+	$('.nav-footer ul.menu>li').after(function(){
+		if(!$(this).hasClass('last-child') && $(this).hasClass('menu-item') && $(this).css('display')!='none'){
+			return '<li class="separator">|</li>';
+		}
+	});
+	
+	$('.section.expandable .expand').click(function(){
+	    var target = $(this).parents('.section-body').find('.content');
+	    console.log(target);
+	    if(target.hasClass('open')){
+            target.removeClass('open');
+            $(this).html('MORE <i class="fa fa-angle-down"></i>');
+	    } else {
+	        target.addClass('open');
+	        $(this).html('LESS <i class="fa fa-angle-up"></i>');
+	    }
+	});
+    if ( $( '.genesis-teaser' ).length ) {
+       $('.genesis-teaser').equalHeightColumns();
+       }
+    if ( $( '.equalize' ).length ) {
+       $('.equalize').equalHeightColumns();
+       }
+});
