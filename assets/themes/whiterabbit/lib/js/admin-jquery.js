d9f8236d81a1e7eea68892665e5bf08777f0dd10
CMO <catherine@msdlab.com>
2017-08-23 12:18:15 -0400

    fix stupid javascript. again. might be time to remove it?

--- assets/themes/whiterabbit/lib/js/admin-jquery.js
+++ assets/themes/whiterabbit/lib/js/admin-jquery.js
@@ -1,10 +1,10 @@
 jQuery(document).ready(function($) {    
    $('textarea').blur(function(){
        var content = $(this).val();
-       var regexp = /href="((?!https?:\/\/).*)"/i;
+       var regexp = /href="([(?!https?:\/\/)|(\#)].*)"/i;
        var matches_array = content.match(regexp);
        if(matches_array.length > 0){
-           alert('You have links that start without http(s)://. Did you mean to do this?');
+           alert('You have links that start without http(s):// or #. Did you mean to do this?');
        }
    });
 });
